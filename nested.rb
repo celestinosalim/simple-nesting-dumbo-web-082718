4356a396679b5b096a5a70014926496969fd41be
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 18:52:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -141,8 +141,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+binding.pry
 adding_to_dennis = programmer_hash[:dennis_ritchie][:languages]
 adding_to_dennis.push["Assembly"]
-binding.pry
+
 end
